48ec532cecacaeadeee47d22201157b7a9201d6b
Alumni SMP PGRI II DEPOK 93 <oktavian.alyandra@gmail.com>
2026-04-20 16:13:44 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,43 @@
-const CACHE_NAME = "stok-warung-v1";
+const CACHE_NAME = "stok-warung-v2";
 
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll([
-        "./",
-        "./index.html"
-      ]);
-    })
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json"
+];
+
+// Install & langsung aktif
+self.addEventListener("install", event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(res => {
-      return res || fetch(e.request);
+// Hapus cache lama otomatis
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
     })
+  );
+  self.clients.claim();
+});
+
+// Ambil data (update otomatis)
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        return response;
+      })
+      .catch(() => caches.match(event.request))
   );
 });
